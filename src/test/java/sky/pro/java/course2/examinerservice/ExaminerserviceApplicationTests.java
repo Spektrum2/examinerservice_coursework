11cb02a7f3b7923e5f1f26bc577499534c629c4e
admin <promitay@gmail.com>
2022-09-02 11:52:20 +0300

    first commit

---
+++ src/test/java/sky/pro/java/course2/examinerservice/ExaminerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course2.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
