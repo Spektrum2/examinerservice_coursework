eaac8d2693049f15f984755bdf66eadd10ddf574
admin <promitay@gmail.com>
2022-09-09 11:06:02 +0300

    Курсовая работа 2-го курса

--- src/main/java/sky/pro/java/course2/examinerservice/service/ExaminerServiceImpl.java
+++ src/main/java/sky/pro/java/course2/examinerservice/service/ExaminerServiceImpl.java
@@ -19,7 +19,7 @@ public class ExaminerServiceImpl implements ExaminerService {
     @Override
     public Collection<Question> getQuestions(int amount) {
         Set<Question> questions = new HashSet<>();
-        Set<Question> questions2 = new HashSet<>();
+        Set<Question> questionsMath = new HashSet<>();
         int countJava;
         int countMath;
         if (amount < 1 || amount > 10) {
@@ -34,10 +34,10 @@ public class ExaminerServiceImpl implements ExaminerService {
         while (questions.size() != countJava) {
             questions.add(questionServices.get(0).getRandomQuestion());
         }
-        while (questions2.size() != countMath) {
-            questions2.add(questionServices.get(1).getRandomQuestion());
+        while (questionsMath.size() != countMath) {
+            questionsMath.add(questionServices.get(1).getRandomQuestion());
         }
-        questions.addAll(questions2);
+        questions.addAll(questionsMath);
         return questions;
     }
 }
